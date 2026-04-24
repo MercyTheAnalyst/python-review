e9a359e162ddb639137985e673780be3eb28a0f5
Mercy <mercyolaleye20@gmail.com>
2026-04-24 13:28:27 +0100

    intro to functions in python

---
+++ Functions.py
@@ -0,0 +1,47 @@
+## I decided to revise my knowlwdfe on pythonas it has been a while I got my hands dirty with it. Here is a quick summary of sample codes on simple built-in functions:
+
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place
+place_up = place.upper()
+
+# Print out place and place_up
+print(place)
+print(place_up)
+
+# Print out the number of o's in place
+place_o = place.count("o")
+print(place_o)
+
+
+## Samples of some methods on lists:
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Use append twice to add poolhouse and garage size
+areas.append(24.5)
+areas.append(15.45)
+
+# Print out areas
+print(areas)
+
+# Reverse the orders of the elements in areas
+areas.reverse()
+
+# Print out areas
+print(areas)
+
+## Methods vs Functions: Unlike standalone functions, methods are called on objects using dot notation. 
+# Import the math package
+import math
+
+# Calculate C
+C = 2 * 0.43 * math.pi
+
+# Calculate A
+A = math.pi * 0.43 ** 2
+
+print("Circumference: " + str(C))
+print("Area: " + str(A))
+
